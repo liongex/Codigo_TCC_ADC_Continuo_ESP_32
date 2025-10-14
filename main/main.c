0d69bb955279b48f25b8db49dfcee2fb1dca2a7a
Liongex <isaclimaf2@gmail.com>
2025-10-13 21:45:06 -0300

    Primeiro commit

---
+++ main/main.c
@@ -0,0 +1,21 @@
+/*
+ * SPDX-FileCopyrightText: 2010-2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: CC0-1.0
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+#include "esp_err.h"
+#include "esp_log.h"
+#include "esp_adc/adc_continuous.h"
+
+
+void app_main(void)
+{
+
+}
