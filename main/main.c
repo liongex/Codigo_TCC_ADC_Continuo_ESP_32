3a85d73c7b819938ab0004f35061f5843911fda3
Liongex <isaclimaf2@gmail.com>
2025-10-13 22:14:11 -0300

    Segundo commit

--- main/main.c
+++ main/main.c
@@ -14,8 +14,25 @@
 #include "esp_log.h"
 #include "esp_adc/adc_continuous.h"
 
+#define EXAMPLE_ADC_UNIT                    ADC_UNIT_1
+#define EXAMPLE_ADC_CONV_MODE               ADC_CONV_SINGLE_UNIT_1
+#define EXAMPLE_ADC_ATTEN                   ADC_ATTEN_DB_6
+#define EXAMPLE_ADC_BIT_WIDTH               SOC_ADC_DIGI_MAX_BITWIDTH
+
+#define EXAMPLE_READ_LEN                    256
+
+static adc_channel_t channel= ADC_CHANNEL_6;
+
+static TaskHandle_t s_task_handle = NULL;
+static const char *TAG = "EXAMPLE";
+
+static bool IRAM_ATTR s_conv_done_cb(adc_continuous_handle_t handle, const adc_continuous_evt_data_t *edata, void *user_data);
+static void continuous_adc_init(adc_channel_t *channel, uint8_t channel_num, adc_continuous_handle_t *out_handle);
+void Task_Adc(void *pvParameters);
 
 void app_main(void)
 {
 
-}
+};
+
+
