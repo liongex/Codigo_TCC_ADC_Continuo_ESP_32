7c0c81a553e18a20dee4df575f5829289e490c06
Liongex <isaclimaf2@gmail.com>
2025-10-13 23:56:42 -0300

    Terceiro commit

--- main/main.c
+++ main/main.c
@@ -19,12 +19,13 @@
 #define EXAMPLE_ADC_ATTEN                   ADC_ATTEN_DB_6
 #define EXAMPLE_ADC_BIT_WIDTH               SOC_ADC_DIGI_MAX_BITWIDTH
 
-#define EXAMPLE_READ_LEN                    256
+#define EXAMPLE_READ_LEN                    400
 
 static adc_channel_t channel= ADC_CHANNEL_6;
 
 static TaskHandle_t s_task_handle = NULL;
 static const char *TAG = "EXAMPLE";
+static uint8_t result[EXAMPLE_READ_LEN] = {0};
 
 static bool IRAM_ATTR s_conv_done_cb(adc_continuous_handle_t handle, const adc_continuous_evt_data_t *edata, void *user_data);
 static void continuous_adc_init(adc_channel_t *channel, uint8_t channel_num, adc_continuous_handle_t *out_handle);
@@ -32,7 +33,112 @@ void Task_Adc(void *pvParameters);
 
 void app_main(void)
 {
+    memset(result, 0xcc, EXAMPLE_READ_LEN);
+
+    xTaskCreatePinnedToCore(
+        Task_Adc,           // Função da tarefa
+        "TarefaNoCore1",        // Nome da tarefa
+        2048,                   // Tamanho da stack (em palavras, não bytes)
+        NULL,                   // Parâmetro para a tarefa
+        2,                      // Prioridade
+        &s_task_handle,   // Handle da tarefa
+        1                       // Core onde a tarefa será fixada (0 ou 1)
+    );
+
 
 };
 
+static bool IRAM_ATTR s_conv_done_cb(adc_continuous_handle_t handle, const adc_continuous_evt_data_t *edata, void *user_data){
+    
+    BaseType_t mustYield = pdFALSE;
+    //Notify that ADC continuous driver has done enough number of conversions
+    vTaskNotifyGiveFromISR(s_task_handle, &mustYield);
+
+    return (mustYield == pdTRUE);
+};
+static void continuous_adc_init(adc_channel_t *channel, uint8_t channel_num, adc_continuous_handle_t *out_handle){
+    
+    adc_continuous_handle_t handle = NULL;
+
+    adc_continuous_handle_cfg_t adc_config = {
+        .max_store_buf_size = 1024,
+        .conv_frame_size = EXAMPLE_READ_LEN,
+    };
+    ESP_ERROR_CHECK(adc_continuous_new_handle(&adc_config, &handle));
+
+    adc_continuous_config_t dig_cfg = {
+        .sample_freq_hz = 6 * 1000,
+        .conv_mode = EXAMPLE_ADC_CONV_MODE,
+    };
+
+    adc_digi_pattern_config_t adc_pattern[SOC_ADC_PATT_LEN_MAX] = {0};
+    dig_cfg.pattern_num = channel_num;
+    for (int i = 0; i < channel_num; i++) {
+        adc_pattern[i].atten = EXAMPLE_ADC_ATTEN;
+        adc_pattern[i].channel = ADC_CHANNEL_6;
+        adc_pattern[i].unit = EXAMPLE_ADC_UNIT;
+        adc_pattern[i].bit_width = EXAMPLE_ADC_BIT_WIDTH;
+
+        ESP_LOGI(TAG, "adc_pattern[%d].atten is :%"PRIx8, i, adc_pattern[i].atten);
+        ESP_LOGI(TAG, "adc_pattern[%d].channel is :%"PRIx8, i, adc_pattern[i].channel);
+        ESP_LOGI(TAG, "adc_pattern[%d].unit is :%"PRIx8, i, adc_pattern[i].unit);
+    }
+    dig_cfg.adc_pattern = adc_pattern;
+    ESP_ERROR_CHECK(adc_continuous_config(handle, &dig_cfg));
+
+    *out_handle = handle;
+
+};
+
+void Task_Adc(void *pvParameters){
+
+    esp_err_t ret;
+    uint32_t ret_num = 0;
+
+    adc_continuous_handle_t handle = NULL;
+    continuous_adc_init(&channel, sizeof(channel) / sizeof(adc_channel_t), &handle);
+
+    adc_continuous_evt_cbs_t cbs = {
+        .on_conv_done = s_conv_done_cb,
+    };
+    ESP_ERROR_CHECK(adc_continuous_register_event_callbacks(handle, &cbs, NULL));
+    ESP_ERROR_CHECK(adc_continuous_start(handle));
+
+    while(1){
+        ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+
+        while(1){
+            ret = adc_continuous_read(handle, result, EXAMPLE_READ_LEN, &ret_num, 0);
+            if (ret == ESP_OK) {
+                ESP_LOGI("TASK", "ret is %x, ret_num is %"PRIu32" bytes", ret, ret_num);
+
+            adc_digi_output_data_t *p = (void *)result;
+
+            for (int i = 0; i < ret_num; i += SOC_ADC_DIGI_RESULT_BYTES) {
+                // Cast direto para acessar a amostra correta
+                const adc_digi_output_data_t *p = (const adc_digi_output_data_t *)(&result[i]);
+                if (p->type1.channel < SOC_ADC_CHANNEL_NUM(ADC_UNIT_1)) {
+                    ESP_LOGI(TAG, "Channel: %d, Value: %d",
+                            p->type1.channel,
+                            p->type1.data);
+                } else {
+                    ESP_LOGW(TAG, "Invalid data [Ch%d_%d]",
+                            p->type1.channel,
+                            p->type1.data);
+                }
+            }
+
+                vTaskDelay(1);
+            } else if (ret == ESP_ERR_TIMEOUT) {
+                //We try to read `EXAMPLE_READ_LEN` until API returns timeout, which means there's no available data
+                break;
+            }
+
+        };
+
+    };
+
+    ESP_ERROR_CHECK(adc_continuous_stop(handle));
+    ESP_ERROR_CHECK(adc_continuous_deinit(handle));
+};
 
